7270c7464fd1cc724da7a95cbbcdb6ad46b38016
George Rodrigues <georgerodrigues@live.com.au>
2025-06-29 22:51:25 -0300

    feat: add markdown-preview

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -3,5 +3,13 @@ return {
    	dependencies = { "MunifTanjim/nui.nvim" },
    	lazy = false,
    	opts = {},
-	}
+	},
+	{ "iamcco/markdown-preview.nvim",
+  	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  	build = "cd app && yarn install",
+        init = function()
+          vim.g.mkdp_filetypes = { "markdown" }
+  	end,
+        ft = { "markdown" },
+	},
 }
