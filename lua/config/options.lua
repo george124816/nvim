7f0cfeb0d74e92656de9156e62c582b4ae3c8558
George Rodrigues <georgerodrigues@live.com.au>
2026-02-04 22:17:50 -0500

    chore: add javascript and configure termguicolors

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,4 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.undofile = true
+vim.opt.termguicolors = true
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -17,6 +17,7 @@ return {
 				"go",
 				"elixir",
 				"kotlin",
+				"javascript"
 			},
 
 			-- Install parsers synchronously (only applied to `ensure_installed`)
